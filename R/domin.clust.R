3344d691bb15e59000fc12746fdad2146d240fd6
alext <alexander.tuzhikov@gmail.com>
2014-10-07 16:24:24 -0400

    win compatability for domin.clust

--- R/domin.clust.R
+++ R/domin.clust.R
@@ -1,11 +1,11 @@
 domin.clust<-function(gen.hclust, g.ids, legend, num.clusters, critical.ratio, sep){
   
-  ctre<-cutree(tree = gen.hclust, k = num.clusters)
+  ctre<-data.frame(cutree(tree = gen.hclust, k = num.clusters))
   #If the group matches cluster
-  names.split=strsplit(x = names(ctre),split = sep)
+  names.split=strsplit(x = row.names(ctre),split = sep)
   groups.mtx<-sapply(1:num.clusters,function(i){
-    li<-sapply(1:length(ctre),function(j){
-      if(i==ctre[j]){
+    li<-sapply(1:nrow(ctre),function(j){
+      if(i==ctre[j,1]){
         return(toString(legend[match(x=as.numeric(names.split[[j]][1]),table = legend[,2]),1]))
       }else{
         return("NotInCluster")
@@ -17,7 +17,7 @@ domin.clust<-function(gen.hclust, g.ids, legend, num.clusters, critical.ratio, s
     return(summary.factor(groups.mtx[,i]))
   })
   cluster.summary<-sapply(cluster.summary,function(i){
-    return(i/length(ctre))
+    return(i/nrow(ctre))
   })
   return(cluster.summary)
 }

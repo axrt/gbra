973f6ad8bcdd2401544b66b4f5f935780edcd8b2
alext <alexander.tuzhikov@gmail.com>
2014-11-20 18:39:12 -0500

    removed print from pca

--- R/pca.R
+++ R/pca.R
@@ -1,7 +1,6 @@
 gen.pca<-function(df, legend, g.ids, sep, trim.names=20, var.axes=FALSE, scale.=FALSE, circle=TRUE, ...){
   check.ggbiplot.loaded()
   groups<-clip.legend(row.names(df),g.ids = g.ids,legend = legend,sep = sep)
-  print(groups)
   colnames(df)<-legend$legend.name
   g<-nice.pca(df = df,legend = legend, groups = groups, var.axes = var.axes, scale. = scale., circle = circle, ... )
   return(g)

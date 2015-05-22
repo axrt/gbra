9696c2e567f9e2731b084d4e5bdcaf4b559b0413
alext <alexander.tuzhikov@gmail.com>
2015-05-22 19:16:20 -0400

    logreg.mismatch.genes debug 4

--- R/logreg.mismatch.genes.R
+++ R/logreg.mismatch.genes.R
@@ -1,7 +1,8 @@
 logreg.mismatch.genes<-function(data, org1, org2, num.bootstraps=0, num.cpu=1, cut=0.5){
   
   #implement checks later
-  working.table<-data[grepl(pattern = paste("\\b[",org1,",",org2,"]X",sep = "",collapse = ""),x = rownames(data),perl = TRUE),c(-org1,-org2)]
+  working.table<-data[grepl(pattern = paste("\\b[",org1,",",org2,"]X",sep = "",collapse = ""),x = rownames(data),perl = TRUE),]
+  working.table<-working.table[,c(-which(colnames(working.table)%in%paste("X",org1,sep="",collapse = "")),-which(colnames(working.table)%in%paste("X",org2,sep="",collapse = "")))]
   working.table$cluster<-grepl(pattern = paste("\\b[",org2,"]X",sep = "",collapse = ""),x = rownames(working.table),perl = TRUE)
   
   f<-paste("cluster ~",paste(colnames(working.table[1:ncol(working.table)-1]),collapse = "+"))

4d5ca101b3c32c0e99aad9814a59f9a9c47b0f9e
alext <alexander.tuzhikov@gmail.com>
2015-06-12 12:28:13 -0400

    read.bh.file added to documentation

--- R/read.bhs.R
+++ R/read.bhs.R
@@ -21,6 +21,19 @@ read.bhs<-function(bh.folder, ext=".short", sep="_"){
   
 }
 
+#' Reads a processed best hit file for a pair of genomes given (see params)
+#' Will install dplyr, tidyr and data.table if not avaliable yet
+#' 
+#' @param \code{qgen.id} query genome id
+#' @param \code{tgen.id} target genome id
+#' @param \code{bh.folder} from a given bh folder
+#' @param \code{ending} with a given file ending, defualt "80.0.short" which means "score cutoff 80"
+#' @param \code{sep} specific character that separates the genome ids and the margin, default "_"
+#' @param \code{margin} a margin symbol that separates the geniome ids, default "VS"
+#' @return a plain data.table representation of the file with "QUERY_ORF_ID", "COMULATIVE_BITSCORE", "ID_QUERY_GENOME" and "ID_TARGET_GENOME"
+#' @examples 
+#' system.time(all.tables<-lapply(genome.ids, function(i){return(read.short.file(i[1],i[3],bh.folder))}))
+#' 
 read.bh.file<-function(qgen.id, tgen.id, bh.folder, ending="80.0.short", sep="_", margin="VS"){
   if(!require("dplyr")){
     install.packages("dplyr")

fdec19bb390ef082d1f9cece30d215496ee0fae0
alext <alexander.tuzhikov@gmail.com>
2015-06-18 17:15:26 -0400

    legend loader corrected

--- R/legend.R
+++ R/legend.R
@@ -1,5 +1,5 @@
 load.legend<-function(legend.file){
   legend<-read.table(file = legend.file, header = TRUE, sep=",")
-  legend<-data.frame(legend$name,legend$id_genomes)
+  legend<-data.frame(name=legend$name,id_genomes=legend$id_genomes)
   return(legend)
 }

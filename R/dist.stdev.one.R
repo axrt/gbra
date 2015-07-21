e316ff92814c0136d8d6f7b3d7125aa27ead519e
axrt <alexander.tuzhikov@gmail.com>
2015-07-21 17:21:04 -0400

    dist.stdev.one commented

--- R/dist.stdev.one.R
+++ R/dist.stdev.one.R
@@ -1,3 +1,18 @@
+#' Normalizes the rows by the standard deviation to make the more or less correlated rows appear closer
+#'@param \code{df} data, can be data.frame or a matrix
+#'@return the data, where each row is normalied by its standard deviation
+#'@examples 
+#'library(plyr)
+#'library(dplyr)
+#'library(tidyr)
+#'bh.data<-read.bhs(bh.folder = "gBLASTer/bh",sep="_") %>% 
+#'  as.data.frame() %>%
+#'  restrict.minimal.hits(minhit = 3) %>%
+#'  normalize.scores() %>%
+#'  attach.genomeid.header() %>%
+#'  sign.bh.table()
+#' data.3.6<-select.genomes(df = bh.data,g.ids = c(3,6))
+#' data.3.6.stn<-dist.stdev.one(data.3.6)
 dist.stdev.one<-function(df){
   
   if(!require("scales")){

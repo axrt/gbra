7f4cbc96a3c37d2ce13032ee053d12bad3cc8a3c
axrt <alexander.tuzhikov@gmail.com>
2015-07-21 11:42:12 -0400

    test finished

--- test/dist.spearman.one.test.R
+++ test/dist.spearman.one.test.R
@@ -1,4 +1,17 @@
-data.spe<-dist.spearman.one(df = data.3.6[1:2,])
+system.time(data.spe<-dist.spearman.one(df = data.3.6))
+system.time(
+  
+  data.spe.r<-apply(data.3.6,1,function(i){
+    return(apply(data.3.6,1,function(j){
+      
+      return(cor(i,j,method="spearman"))
+      
+    }))
+  })
+  
+)
+
+
 data.3.6<-as.matrix(data.3.6)
 head(as.matrix(data.spe))
 cor(data.3.6[1,],data.3.6[2,],method="spearman")

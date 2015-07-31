a0df2693368e7e2c8da7c81de4de889f249b6ad6
axrt <alexander.tuzhikov@gmail.com>
2015-07-31 12:54:38 -0400

    load.hits table commented

--- R/hits.R
+++ R/hits.R
@@ -1,3 +1,7 @@
+#'Use to load hits table
+#'@param \code{hits.file} a path to the hits file
+#'@return a \code{dataframe} with the data
+#'
 load.hits<-function(hits.file){
   hits<-read.table(file = hits.file,header = TRUE,sep = "\t",row.names = 1)
   return(hits)

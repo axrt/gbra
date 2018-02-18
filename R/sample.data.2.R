dde797f8c2ae9777094b5b05ee4d4e5023f0ad31
axrt <alexander.tuzhikov@gmail.com>
2018-02-18 10:02:15 -0800

    some uncommited stuff

--- R/sample.data.2.R
+++ R/sample.data.2.R
@@ -11,15 +11,16 @@ sample.data.2<- function(data, part=0.75){
     mutate(names=rownames(data)) %>% 
     group_by(cluster) %>% 
     do({
-      .[sample(x = nrow(.),size = nrow(.)*part,replace = FALSE),]
-    })
+      df<-.
+      return(df[sample(x = 1:nrow(df), size = nrow(df)*part,replace = FALSE),])
+    }) %>% data.frame
   
   rest.of.data<- data[!rownames(data)%in%sampled.data$names,] 
   rownames(sampled.data)<- sampled.data$names
   
   return(
     list(
-      train=select(sampled.data,-c(names)),
+      train=sampled.data[,!colnames(sampled.data) %in% c("names")],
       validate=rest.of.data
     )
   )
